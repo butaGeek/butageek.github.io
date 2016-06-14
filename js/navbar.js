f1647281cab983c03b463d5a300ce1df9bad8e8d
Hendry Zhou <hendryzhou889@gmail.com>
2016-06-14 00:08:33 -0400

    update layout

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
+$(function() {
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
+
+  var aboutTop = $('#about').offset().top;
+
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > aboutTop) {
+      $('#up').addClass('visible');
+    } else {
+      $('#up').removeClass('visible');
+    }
+  })
 });
